45922c27dd15ddc61bb570865fc33bf65e17243d
jocoso <joshua.collado022@gmail.com>
2023-03-25 14:08:28 -0400

    Added a good picture

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,17 @@
 
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 
 const IndexPage = () => {
     return (
         <Layout pageTitle="Home Page">
             <p>This is my artist blog</p>
+            <StaticImage
+                alt="A piece of art I made myself"
+                src="../images/godofdeep.png"
+            />
         </Layout>
     )
 }
