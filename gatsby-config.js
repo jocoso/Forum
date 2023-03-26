28cf4352ce1120966c8e91878696c00f5026091a
jocoso <joshua.collado022@gmail.com>
2023-03-26 00:30:56 -0400

    Added some excerpts because why not?

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,8 @@ module.exports = {
         // theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+     },
       },
-    },
+    "gatsby-plugin-mdx",
   ],
 }
